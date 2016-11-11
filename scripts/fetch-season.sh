b5598d172d054e07b15ef7dd24c35a87ea83a2d6
Adam Wentz <adam.wentz@dollarshaveclub.com>
2016-11-10 21:21:49 -0800

    You know me, I gotta put in a big tree.

--- scripts/fetch-season.sh
+++ scripts/fetch-season.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 season_urls=(
-  ""
+  "not a season"
   "https://www.youtube.com/playlist?list=PLAEQD0ULngi69x_7JbQvSMprLRK_KSVLu"
-  "https://www.youtube.com/playlist?list=PLAEQD0ULngi69x_7JbQvSMprLRK_KSVLu"
+  "https://www.youtube.com/playlist?list=PLAEQD0ULngi5VAEOviVE6svrUW2axISf6"
+  "https://www.youtube.com/playlist?list=PLAEQD0ULngi7zDD6O36FKkEHse-JCdVvh"
+  "https://www.youtube.com/playlist?list=PLAEQD0ULngi56KMlB1P_wK9pJZcMpEFQd"
+  "https://www.youtube.com/playlist?list=PLAEQD0ULngi6bAFRfcqgpKP4T4SnoxoAz"
 )
 URL=${season_urls[$1]}
 
