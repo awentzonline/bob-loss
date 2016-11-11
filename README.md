8b12bbb22026da829f2f2af4538549dd8cb4b86a
Adam Wentz <adam.wentz@dollarshaveclub.com>
2016-11-10 20:43:20 -0800

    I taught my son to paint mountains like these, and guess what? Now he paints the best darn mountains in the industry.

---
+++ README.md
@@ -0,0 +1,3 @@
+Bob Loss, neural painter
+========================
+We don't make mistakes, we just have happy accidents.
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+    name='bob-loss-neural-painter',
+    version='0.0.1',
+    description='Bob Loss, neural painter',
+    author='Adam Wentz',
+    author_email='adam@adamwentz.com',
+    url='https://github.com/awentzonline/bob-loss',
+    packages=[
+        'bobloss'
+    ]
+)
